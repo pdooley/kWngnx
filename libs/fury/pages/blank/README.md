f577a67c9d1d18b3586e70692539a0ebee4dbc65
Patrick.Dooley <pjldooley@gmail.com>
2022-06-09 01:24:41 -0400

    dd

--- libs/fury/pages/blank/README.md
+++ libs/fury/pages/blank/README.md
@@ -1,10 +1,10 @@
-# fury-pages-coming-soon
+# fury-pages-blank
 
 This library was generated with [Nx](https://nx.dev).
 
 
 ## Running unit tests
 
-Run `nx test fury-pages-coming-soon` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test fury-pages-blank` to execute the unit tests via [Jest](https://jestjs.io).
 
 
--- libs/fury/pages/blank/jest.config.js
+++ libs/fury/pages/blank/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'fury-pages-coming-soon',
+  displayName: 'fury-pages-blank',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$':  'ts-jest'
   },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/fury/pages/coming-soon'
+  coverageDirectory: '../../../../coverage/libs/fury/pages/blank'
 };
