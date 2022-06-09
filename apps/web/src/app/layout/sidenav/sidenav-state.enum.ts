cfb81f0449cc927780e1363a3266c05a11abaa67
Patrick Dooley <pdooley@vectorsolv.com>
2022-06-09 14:07:20 -0400

    aa

--- apps/web/src/app/layout/sidenav/sidenav-state.enum.ts
+++
@@ -1,8 +0,0 @@
-export enum SidenavState {
-  Fixed = 'fixed',
-  Expanded = 'expanded',
-  Collapsed = 'collapsed',
-  CollapsedHover = 'collapsedHover',
-  Mobile = 'mobile',
-  MobileOpen = 'mobileOpen'
-}
