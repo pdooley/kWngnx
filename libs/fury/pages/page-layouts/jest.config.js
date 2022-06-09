24f6cf4d359f96ab1bbf6a376e58ce977f2cca4e
Patrick.Dooley <pjldooley@gmail.com>
2022-06-09 01:20:43 -0400

    dd

---
+++ libs/fury/pages/page-layouts/jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  displayName: 'fury-pages-page-layouts',
+  preset: '../../../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    }
+  },
+  transform: {
+    '^.+\\.[tj]sx?$':  'ts-jest'
+  },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../../../coverage/libs/fury/pages/page-layouts'
+};
