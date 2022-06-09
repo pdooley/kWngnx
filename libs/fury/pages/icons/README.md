657d0d37bddbad6ccaebccabaab8eadfeb83d35a
Patrick.Dooley <pjldooley@gmail.com>
2022-06-09 01:36:55 -0400

    dd

--- libs/fury/pages/icons/README.md
+++ libs/fury/pages/icons/README.md
@@ -1,10 +1,10 @@
-# fury-pages-icon
+# fury-pages-icons
 
 This library was generated with [Nx](https://nx.dev).
 
 
 ## Running unit tests
 
-Run `nx test fury-pages-icon` to execute the unit tests via [Jest](https://jestjs.io).
+Run `nx test fury-pages-icons` to execute the unit tests via [Jest](https://jestjs.io).
 
 
--- libs/fury/pages/icons/jest.config.js
+++ libs/fury/pages/icons/jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  displayName: 'fury-pages-icon',
+  displayName: 'fury-pages-icons',
   preset: '../../../../jest.preset.js',
   globals: {
     'ts-jest': {
@@ -10,5 +10,5 @@ module.exports = {
     '^.+\\.[tj]sx?$':  'ts-jest'
   },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../../coverage/libs/fury/pages/icon'
+  coverageDirectory: '../../../../coverage/libs/fury/pages/icons'
 };
